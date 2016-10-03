42c59117c04756d850c8f707e00fdeb9280f50d6
Aditya2202 <aditya4bel@gmail.com>
2016-10-03 09:20:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 //Move the image
 var img= document.getElementById('madi');
-img.onclick= function(){
-    img.style.marginLeft ='100px';
+var marginLeft =0;
+function moveRight(){
+    marginLeft = marginLeft +10;
+    img.style.marginLeft = marginLeft + 'px';
 }
+img.onclick= function(){
+    var interval = setInterval(moveRight, 100);
+  }
